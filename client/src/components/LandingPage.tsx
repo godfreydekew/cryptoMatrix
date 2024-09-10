df6c305223d9f24a9e1c42ce0248c954bca68abc
felabel <felicityabel2016@gmail.com>
2024-09-10 08:13:19 +0100

    used a mock api from jsonplaceholder

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,10 +1,12 @@
 // src/components/LandingPage.jsx
 import React from 'react'
+import Users from './Users/users'
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Welcome to the Landing Page!</h1>
+      <Users />
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-import './index.css'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
