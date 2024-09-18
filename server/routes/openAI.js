c43caabf691ae9d8dbfe5fdffad87ae783f65fcd
godfreydekew <dekewgodfrey@gmail.com>
2024-09-18 18:41:40 +0300

    Fix chatbot errors

--- server/routes/openAI.js
+++ server/routes/openAI.js
@@ -5,18 +5,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const router = express.Router();
-
+const OPENROUTER_API_KEY = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: OPENROUTER_API_KEY,
+  // defaultHeaders: {
+  //   "HTTP-Referer": $YOUR_SITE_URL, // Optional, for including your app on openrouter.ai rankings.
+  //   "X-Title": $YOUR_SITE_NAME, // Optional. Shows in rankings on openrouter.ai.
+  // }
+})
 
 router.post('/gpt', async (req, res) => {
   try {
     const { message } = req.body;
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: message }],
+      model: "openai/gpt-4o-mini",//gpt-3.5-turbo
+      messages: [{ role: "user", content: message }]
     });
 
     res.json({ message: completion.choices[0].message.content });
@@ -25,5 +30,22 @@ router.post('/gpt', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while processing your request.' });
   }
 });
+
+
+// router.get('/limits', async(req, res) => {
+//   try {
+//     const response = await axios.get('https://openrouter.ai/api/v1/models', {
+//       headers: {
+//         Authorization: `Bearer ${OP}`,  // Use your API key here
+//       },
+//     });
+
+//     // Send the response data back to the client
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error('Error in limits route:', error);
+//     res.status(500).json({ error: 'An error occurred while processing your request.' });
+//   }
+// })
 
 export default router;
