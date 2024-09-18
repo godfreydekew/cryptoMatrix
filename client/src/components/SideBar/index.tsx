958aabf2651d19861c467fd532613e39b444f6f2
felabel <felicityabel2016@gmail.com>
2024-09-18 23:28:33 +0100

    implemented mobile and desktop responsiveness

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -1,19 +1,19 @@
-// SideBar.tsx
 import React, { useState } from 'react'
 import './style.scss'
 import { CgMenuGridR } from 'react-icons/cg'
 import { IoPieChartOutline } from 'react-icons/io5'
-import { IoWalletOutline } from 'react-icons/io5'
 import { GrTransaction } from 'react-icons/gr'
 import { RiNewsLine } from 'react-icons/ri'
 import { BiLogOut } from 'react-icons/bi'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { logout } from '../../api/api'
+import { FaBars } from 'react-icons/fa' // Menu icon
 
 const SideBar = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const [activeLink, setActiveLink] = useState(location.pathname)
+  const [isMenuOpen, setIsMenuOpen] = useState(false) // Track menu state
 
   const navLinks = [
     { name: 'Overview', icon: <CgMenuGridR />, link: '/dashboard' },
@@ -23,7 +23,6 @@ const SideBar = () => {
       icon: <GrTransaction />,
       link: '/dashboard/transactions',
     },
-    // { name: 'Wallet', icon: <IoWalletOutline />, link: '/dashboard' },
     { name: 'News', icon: <RiNewsLine />, link: '/news' },
   ]
 
@@ -32,38 +31,52 @@ const SideBar = () => {
       const response = await logout()
       navigate('/')
       console.log('Logout successful:', response)
-      // Optionally redirect or show a message after successful logout
     } catch (error) {
       console.error('Logout failed:', error)
     }
   }
 
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen) // Toggle menu open/close
+  }
+
+  const handleLinkClick = (link: any) => {
+    setActiveLink(link)
+    navigate(link)
+    setIsMenuOpen(false) // Close menu after selecting link
+  }
+
   return (
-    <div className="pd_sidebar_wrapper">
-      <div className="inner_wrap">
-        <div className="logo">
-          <h2>cryptoMatrix</h2>
-        </div>
-        {navLinks.map((item, i) => (
-          <div
-            className={`nav_flex ${activeLink === item.link ? 'active' : ''}`}
-            key={i}
-            onClick={() => {
-              setActiveLink(item.link) // Update active link
-              navigate(item.link)
-            }}>
-            <div className="icon">{item.icon}</div>
-            <p>{item.name}</p>
+    <>
+      {/* Menu icon for mobile view */}
+      <div className="menu-icon" onClick={handleMenuClick}>
+        <FaBars size={24} />
+      </div>
+
+      {/* Sidebar */}
+      <div className={`pd_sidebar_wrapper ${isMenuOpen ? 'open' : ''}`}>
+        <div className="inner_wrap">
+          <div className="logo">
+            <h2>CryptoMatrix</h2> {/* Logo on mobile screens */}
           </div>
-        ))}
-        <div className="nav_flex bottom" onClick={handleLogout}>
-          <div className="icon">
-            <BiLogOut />
+          {navLinks.map((item, i) => (
+            <div
+              className={`nav_flex ${activeLink === item.link ? 'active' : ''}`}
+              key={i}
+              onClick={() => handleLinkClick(item.link)}>
+              <div className="icon">{item.icon}</div>
+              <p>{item.name}</p>
+            </div>
+          ))}
+          <div className="nav_flex bottom" onClick={handleLogout}>
+            <div className="icon">
+              <BiLogOut />
+            </div>
+            <p>LogOut</p>
           </div>
-          <p>LogOut</p>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- server/routes/bybitRoutes.js
+++ server/routes/bybitRoutes.js
@@ -1,19 +1,22 @@
 //routes//bybitRoutes.js
-import express from 'express';
-import { fetchAllAssets, getTotalBalance, getAllTransactions } from '../controllers/bybitController.js';
-import { isAuthenticated } from '../middleware/auth.js'; 
+import express from "express";
+import {
+  fetchAllAssets,
+  getTotalBalance,
+  getAllTransactions,
+} from "../controllers/bybitController.js";
+import { isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.use(isAuthenticated);
+// router.use(isAuthenticated);
 // Route to get total balance in USD
-router.get('/total_balance', getTotalBalance);
+router.get("/total_balance", getTotalBalance);
 
 // Route to get all transactions in the last 100 days
-router.get('/transactions', getAllTransactions);
-
+router.get("/transactions", getAllTransactions);
 
 // Route to get all assets with price
-router.get('/assets', fetchAllAssets);
+router.get("/assets", fetchAllAssets);
 
 export default router;
