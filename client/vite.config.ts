b3f668664ced9b4a68f1d0c23b754a38ae02e51d
godfreydekew <dekewgodfrey@gmail.com>
2024-09-23 17:47:46 +0300

    Add public/ in the client to handle redirects

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Make sure the build output goes to dist
-  }
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
 })
